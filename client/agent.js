c6262b61bafcab1c2cb61528bae9a52b157b4662
asyst.dnb@gmail.com <asyst.dnb@gmail.com>
2018-11-22 11:33:24 +0200

    fixed untracked files

--- client/agent.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const Profile = {
-    fetchProfilePicture: (uid) => axios.get(`https://graph.facebook.com/${uid}/picture?width=120&height=120`)
-};
-
-export default {
-    Posts
-};
--- client/components/BreadCrumbs/BreadCrumbs.js
+++
@@ -1,7 +0,0 @@
-import { Breadcrumb } from 'antd';
-
-const BreadCrumbs = ({ crumbs }) => <Breadcrumb style={{ margin: '74px 0px 14px' }}>
-        { crumbs.map(crumb => crumb !== "" && <Breadcrumb.Item key={ crumb }>{ crumb }</Breadcrumb.Item>) }
-    </Breadcrumb>
-
-export default BreadCrumbs;
